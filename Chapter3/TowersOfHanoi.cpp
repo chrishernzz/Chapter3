9b7810949dda3c852d332d45ce9d821936aa27cd
Christian Hernandez <christianhernandez1014@gmail.com>
2023-10-20 11:04:22 -0700

    Finish

--- Chapter3/TowersOfHanoi.cpp
+++ Chapter3/TowersOfHanoi.cpp
@@ -63,8 +63,6 @@ void TowersOfHanoi::menuChallenge(){
 	char playAgain;
 	int ringsNumber;
 	int moves, discsTotalNumber = 64;
-	//using vector STL for my all 3 pegs of the disc
-	//vector<int> pegA, pegB, pegC;
 	char choicePeg1, choicePeg2;
 	//going to use map STL to keep track of each moves and how fast they go, and total moves, and the game counts as well
 	map<int, int> fastTime, slowTime, gameCount, movesEachGameFast, movesEachGameSlow;
